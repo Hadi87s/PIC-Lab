8d77dbdfaa15042cdbb4dd1792b15ed6ce97b19f
Hadi87s <HadiIrshaid8722@gmail.com>
2025-02-13 13:22:09 +0200

    adding project files

---
+++ firmware/Samaritan.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Samaritan.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Samaritan.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=samaritan.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/samaritan.x.tar
